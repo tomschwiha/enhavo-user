d1dbd37385666a5ce722aa1376f65a90a70660f6
Tom Schwiha <tom@schwiha.de>
2023-06-20 12:04:45 +0200

    Initial commit

---
+++ Encore/EncoreRegistryPackage.js
@@ -0,0 +1,23 @@
+
+class EncoreRegistryPackage
+{
+    constructor(config = null)
+    {
+        this.config = {};
+        if(this.config !== null) {
+            this.config = config;
+        }
+    }
+
+    initEncore(Encore, name)
+    {
+
+    }
+
+    initWebpackConfig(config, name)
+    {
+
+    }
+}
+
+module.exports = EncoreRegistryPackage;
